fac7aebe761112ce837317e461126f5b07ee1309
dsfackrell <>
2021-06-04 00:36:11 +0000

    Completed Part 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -2,12 +2,13 @@ const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName;
+// TODO 1.1a: Define candidateName //
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let spacer = "\n-----------------------\n";
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,22 +16,29 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  console.log();
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer = input.question("1) " + question);  
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  console.log(spacer);
+  if (correctAnswer === candidateAnswer) {
+    console.log("Your Answer: " + candidateAnswer);
+    console.log("That is the correct answer.");
+  } else {
+    console.log("Your Answer: " + candidateAnswer);
+    console.log("Sorry that is not correct.");
+  }
+  console.log(spacer);
 
-
-  let grade;
-  
+  let grade;  
 
   return grade;
 }
@@ -38,6 +46,9 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
+  console.log(spacer);
+  console.log("Hello " + candidateName + " Welcome to Candidate Testing.");
+  console.log(spacer);
   
   askQuestion();
   gradeQuiz(this.candidateAnswers);
