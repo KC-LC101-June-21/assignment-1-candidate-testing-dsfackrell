340632edfaba923d490b96fbb6656ed86bb893bc
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2021-06-11 00:31:26 +0000

    Finished Part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,10 +9,15 @@ let spacer = "\n-----------------------\n";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];;
+let candidateAnswers = [];
+let questions = [
+  "Who was the first American woman is space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
+];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -22,19 +27,24 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question("1) " + question);  
+  //candidateAnswer = input.question("1) " + question);  
+  for(i = 0; i < questions.length; i++) {
+    candidateAnswers.push(input.question((i + 1) + ") " + questions[i]));
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   console.log(spacer);
-  if (correctAnswer === candidateAnswer) {
-    console.log("Your Answer: " + candidateAnswer);
-    console.log("That is the correct answer.");
-  } else {
-    console.log("Your Answer: " + candidateAnswer);
-    console.log("Sorry that is not correct.");
+  for(i = 0; i < questions.length; i++) {
+    console.log(`For question ${i + 1}) you entered: ${candidateAnswers[i]}.`);
+    if (candidateAnswers[i] === correctAnswers[i]) {
+      console.log("That is the correct answer.");
+    } else {
+      console.log(`Incorrect, the correct answer is: ${correctAnswers[i]}`);
+    }
+    console.log();
   }
   console.log(spacer);
 
