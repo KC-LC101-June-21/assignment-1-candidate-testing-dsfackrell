a9499b17ab9d299849918289af3736574a5c72b0
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2021-06-16 00:51:08 +0000

    test

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,11 +12,11 @@ let candidateAnswer = "";
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];;
 let candidateAnswers = [];
 let questions = [
-  "Who was the first American woman is space?",
-  "True or false: 5 kilometer == 5000 meters?",
-  "(5 + 3)/2 * 10 = ?",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
-  "What is the minimum crew size for the ISS?"
+  "Who was the first American woman is space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "
 ];
 
 function askForName() {
@@ -36,7 +36,7 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade = 0;  
+  let grade;  
   console.log(spacer);
   for(i = 0; i < questions.length; i++) {
     console.log(`For question ${i + 1}) you entered: ${candidateAnswers[i]}.`);
