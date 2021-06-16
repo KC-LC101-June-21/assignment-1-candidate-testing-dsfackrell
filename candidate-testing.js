b51c022879b697ec76debd97263da287048f22c7
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2021-06-16 00:34:34 +0000

    Part 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,7 +6,7 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let spacer = "\n-----------------------\n";
-let question = "Who was the first American woman in space? ";
+let question = 'Who was the first American woman in space? ';
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];;
