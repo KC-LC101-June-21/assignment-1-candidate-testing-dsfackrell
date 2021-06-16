9ae0f367bd623de6450cc8b8ac59be958f11e818
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2021-06-16 00:56:09 +0000

    Removed Trailing Spaces in questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,17 +6,17 @@ const input = require('readline-sync');
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let spacer = "\n-----------------------\n";
-let question = 'Who was the first American woman in space? ';
+let question = 'Who was the first American woman in space?';
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];;
 let candidateAnswers = [];
 let questions = [
-  "Who was the first American woman is space? ",
-  "True or false: 5 kilometer == 5000 meters? ",
-  "(5 + 3)/2 * 10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-  "What is the minimum crew size for the ISS? "
+  "Who was the first American woman is space?",
+  "True or false: 5 kilometer == 5000 meters?",
+  "(5 + 3)/2 * 10 = ?",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?",
+  "What is the minimum crew size for the ISS?"
 ];
 
 function askForName() {
